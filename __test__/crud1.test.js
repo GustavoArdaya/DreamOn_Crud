722117d8e258df70807d8ca78c1eda3c66270a47
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-01-18 12:54:44 +0100

    created 10 digits id number randomizer with Math.random method

--- __test__/crud1.test.js
+++ __test__/crud1.test.js
@@ -4,8 +4,8 @@ const { itemList } = require("../src/crud1")
 test('should accept a list input and store it into object list', () => {
     //GIVEN
     const list = [
-        { id: 321498798, title: "item1", imgUrl: "ljsldjfsljf.jpg"}, 
-        { id: 321498578, title: "item2", imgUrl: "ljsldjfsljf.jpg"}
+        { id: 3214987982, title: "item1", imgUrl: "ljsldjfsljf.jpg"}, 
+        { id: 3214985785, title: "item2", imgUrl: "ljsldjfsljf.jpg"}
     ]
     //WHEN
     const crud = itemList.initialize(list);
@@ -17,13 +17,27 @@ test('should accept a list input and store it into object list', () => {
 test('should delete item from list', () => {
     //GIVEN
     const list = [
-        { id: 321498798, title: "item1", imgUrl: "ljsldjfsljf.jpg"}, 
-        { id: 321498578, title: "item2", imgUrl: "ljsldjfsljf.jpg"}
+        { id: 3214987982, title: "item1", imgUrl: "ljsldjfsljf.jpg"}, 
+        { id: 3214985785, title: "item2", imgUrl: "ljsldjfsljf.jpg"}
     ]
     //WHEN
     const crud = itemList.initialize(list); // this adds items into list to work with
-    crud.deleteById(321498798)
+    crud.deleteById(3214987982)
     const sut = crud.list;
     //THEN
     expect(sut.length).toEqual(1);
+})
+
+test('should add item into list', () => {
+    //GIVEN
+    const list = [
+        { id: 3214987982, title: "item1", imgUrl: "ljsldjfsljf.jpg"}, 
+        { id: 3214985785, title: "item2", imgUrl: "ljsldjfsljf.jpg"}
+    ]
+    //WHEN
+    const crud = itemList.initialize(list); // this adds items into list to work with
+    crud.addNewItem({ title: "title", imgUrl: "imagenURL.jpg"} );
+    const sut = crud.list;
+    //THEN
+    expect(sut.length).toEqual(3);
 })
--- src/crud1.js
+++ src/crud1.js
@@ -2,7 +2,7 @@ const itemList = {
     list: [],
     createItem() {
         let newObject = {
-            id: "",
+            id: Math.floor((Math.random() * 10**10)),
             title: "nameless item",
             imgUrl: "https://static.wixstatic.com/media/ea71bb_5c8f1a2d38954c5d8422d8d706593d54~mv2.png/v1/fill/w_872,h_873,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/ea71bb_5c8f1a2d38954c5d8422d8d706593d54~mv2.png"
         }
@@ -15,8 +15,24 @@ const itemList = {
         let index = this.list.findIndex((item) => item.id == id);
         this.list.splice(index, 1);
     },
+    addNewItem(item) {
+        let newItem = {
+            title: item.title,
+            imgUrl: item.imgUrl
+        }
+    }
 }
 
 module.exports = { itemList }
 
 //console.log(crud.addItemtoList());
+
+
+let randomIdItem = {
+    id: Math.floor((Math.random() * 10**10)),
+    title: "nameless item",
+    imgUrl: "https://static.wixstatic.com/media/ea71bb_5c8f1a2d38954c5d8422d8d706593d54~mv2.png/v1/fill/w_872,h_873,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/ea71bb_5c8f1a2d38954c5d8422d8d706593d54~mv2.png"
+}
+ console.log(randomIdItem)
+
+
